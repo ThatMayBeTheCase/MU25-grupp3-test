97d830fa5ca9470a00c53cd07580de75a735e871
Student <student@exempel.se>
2025-10-22 10:25:35 +0200

    #US3 Implement active/inactive btn with color

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const Katalog = [
-  { name: "Kalle", age: 20, active: true },
-  { name: "Pelle", age: 22, active: false },
-  { name: "Erik", age: 21, active: true },
+  { name: "Kalle Anderson", age: 20, active: true },
+  { name: "Pelle Larsson", age: 22, active: false },
+  { name: "Erik Svensson", age: 21, active: true },
 ];
 
 const ul = document.getElementById("student-lista");
 
-Katalog.forEach(({ name, age }) => {
+Katalog.forEach(({ name, age, active }) => {
   const li = document.createElement("li");
-  li.innerHTML =
-  `
+  li.className = active ? 'active-student' : 'inactive-student';
+  li.innerHTML = `
     <span class="name">${name}</span>
     <span class="age">Age ${age}</span>
+    <span class="status ${active ? 'active' : 'inactive'}">
+        ${active ? 'Aktiv' : 'Inaktiv'}
+    </span>
   `;
   ul.appendChild(li);
 });
